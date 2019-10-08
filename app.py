97c7c9f9e219d12e0160ec63f8b280a7b74ae6e9
James Swank <jmswank7@gmail.com>
2019-10-08 11:09:28 -0600

    starts new project

---
+++ app.py
@@ -0,0 +1,13 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table as dt
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
+from datetime import datetime, date, timedelta
+import json, csv, dash_table, time, operator
+from conect import norm_records, rec_lows, rec_highs, all_temps
+import pandas as pd
+from numpy import arange,array,ones
+from scipy import stats 
+import psycopg2
