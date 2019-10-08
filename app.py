dead1081ce2c2dfd4e69a09c52718e47665b21f5
James Swank <jmswank7@gmail.com>
2019-10-08 11:39:09 -0600

    adds dynamic title date

--- app.py
+++ app.py
@@ -15,7 +15,21 @@ import psycopg2
 
 app = dash.Dash(__name__)
 app.config['suppress_callback_exceptions']=True
-# print(all_temps)
+
+df_all_temps = pd.DataFrame(all_temps,columns=['dow','sta','Date','TMAX','TMIN'])
+
+
+
+df_norms = pd.DataFrame(norm_records)
+
+df_rec_lows = pd.DataFrame(rec_lows)
+
+df_rec_highs = pd.DataFrame(rec_highs)
+
+current_year = datetime.now().year
+today = time.strftime("%Y-%m-%d")
+startyr = 1950
+year_count = current_year-startyr
 
 app.layout = html.Div(
     [
@@ -28,8 +42,72 @@ app.layout = html.Div(
         ],
             className='row'
         ),
+        html.Div([
+            html.H4(
+                id='title-date-range',
+                className='twelve columns',
+                style={'text-align': 'center'}
+            ),
+        ],
+            className='row'
+        ),
+        html.Div([
+            html.Div([
+                html.Label('Select Product'),
+                dcc.RadioItems(
+                    id='product',
+                    options=[
+                        {'label':'Temperature graphs', 'value':'temp-graph'},
+                        {'label':'Climatology for a day', 'value':'climate-for-day'}
+                    ],
+                    value='temp-graph',
+                    labelStyle={'display': 'block'},
+                ),
+            ],
+                className='three columns',
+            ),
+            html.Div([
+                html.Label('Options'),
+                html.Div(
+                    id='period-picker'
+                ),
+                html.Div(
+                    id='year-picker'
+                ),
+                html.Div(
+                    id='date-picker'
+                ),
+            ],
+                className='two-columns',
+            ),  
+        ],
+            className='row'
+        ),
+        html.Div(id='all-data', style={'display': 'none'}),
     ]
 )
+
+@app.callback(Output('all-data', 'children'),
+            [Input('product', 'value')])
+def all_temps_cleaner(product_value):
+    # print(product_value)
+    cleaned_all_temps = df_all_temps
+    cleaned_all_temps.columns=['dow','sta','Date','TMAX','TMIN']
+    cleaned_all_temps['Date'] = pd.to_datetime(cleaned_all_temps['Date'])
+    cleaned_all_temps = cleaned_all_temps.drop(['dow','sta'], axis=1)
+
+    return cleaned_all_temps.to_json(date_format='iso')
+
+@app.callback(Output('title-date-range', 'children'),
+            [Input('product', 'value'),
+            Input('all-data', 'children')])
+def all_temps_cleaner(product, temps):
+    print(product)
+    title_temps = pd.read_json(temps)
+    title_temps['Date']=title_temps['Date'].dt.strftime("%Y-%m-%d")
+    last_day = title_temps.iloc[-1, 0] 
+    
+    return '1950-01-01 through {}'.format(last_day)
 
 if __name__ == "__main__":
     app.run_server(port=8050, debug=True)
