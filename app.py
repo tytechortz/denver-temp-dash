c407fc2958b7fdd1942485cc4686d455be9708c0
James Swank <jmswank7@gmail.com>
2019-10-08 11:16:25 -0600

    gets data from db

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 from datetime import datetime, date, timedelta
 import json, csv, dash_table, time, operator
-from conect import norm_records, rec_lows, rec_highs, all_temps
+from connect import norm_records, rec_lows, rec_highs, all_temps
 import pandas as pd
 from numpy import arange,array,ones
 from scipy import stats 
 import psycopg2
 
+print(all_temps)
