0d20c50038ba92f05cfc98eb1618193a72459cf4
James Swank <jmswank7@gmail.com>
2019-10-13 21:33:06 -0600

    start graph stats and clean up unused code

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ today = time.strftime("%Y-%m-%d")
 startyr = 1950
 year_count = current_year-startyr
 
-print(type(df_all_temps['Date'][0]))
 
 def get_layout():
     return html.Div(
@@ -94,7 +93,15 @@ def get_layout():
                     ),
                 ],
                     className='eight columns'
-                ),    
+                ),
+                html.Div([
+                    html.Div([
+                        html.Div(id='graph-stats'
+                        ),
+                    ],
+                        className='four columns'
+                    ),
+                ])    
             ],
                 className='row'
             ),
@@ -149,6 +156,7 @@ def get_layout():
             html.Div(id='d-min-min', style={'display': 'none'}),
             html.Div(id='avg-of-dly-lows', style={'display': 'none'}),
             html.Div(id='d-max-min', style={'display': 'none'}),
+            html.Div(id='day-count', style={'display': 'none'}),
         ]
     )
 
@@ -157,11 +165,197 @@ app.layout = get_layout
 app.config['suppress_callback_exceptions']=True
 
 @app.callback(
-            Output('daily-max-t', 'children'),
-            [Input('product', 'value'),
-            Input('d-max-max', 'children'),
-            Input('avg-of-dly-highs', 'children'),
-            Input('d-min-max', 'children')])
+    Output('graph-stats', 'children'),
+    [Input('day-count', 'children')])
+def graph_stats(day_count):
+    if product == 'temp-graph':
+        return html.Div([
+            html.Div([
+                html.H6('Day Count', style={'text-align':'center'}),
+                html.H6('{}'.format(day_count), style={'text-align': 'center'})
+            ])
+        ])
+
+@app.callback(
+    Output('day-count', 'children'),
+    [Input('temp-data', 'children')])
+def graph_stats(temp_data):
+    day_count = 10
+    return day_count.to_json()
+
+
+@app.callback(Output('graph1', 'figure'),
+             [Input('temp-data', 'children'),
+             Input('rec-highs', 'children'),
+             Input('rec-lows', 'children'),
+             Input('norms', 'children'),
+             Input('year', 'value'),
+             Input('period', 'value')])
+def update_figure(temp_data, rec_highs, rec_lows, norms, selected_year, period):
+    previous_year = int(selected_year) - 1
+    selected_year = selected_year
+    temps = pd.read_json(temp_data)
+    temps = temps.drop([0,1], axis=1)
+    temps.columns = ['date','TMAX','TMIN']
+    temps['date'] = pd.to_datetime(temps['date'], unit='ms')
+    temps = temps.set_index(['date'])
+    temps['dif'] = temps['TMAX'] - temps['TMIN']
+    
+    temps_cy = temps[(temps.index.year==selected_year)]
+    temps_py = temps[(temps.index.year==previous_year)]
+    df_record_highs_ly = pd.read_json(rec_highs)
+    df_record_highs_ly = df_record_highs_ly.set_index(1)
+    df_record_lows_ly = pd.read_json(rec_lows)
+    df_record_lows_ly = df_record_lows_ly.set_index(1)
+    df_rl_cy = df_record_lows_ly[:len(temps_cy.index)]
+    df_rh_cy = df_record_highs_ly[:len(temps_cy.index)]
+
+    df_norms = pd.read_json(norms)
+    df_norms_cy = df_norms[:len(temps_cy.index)]
+  
+    temps_cy.loc[:,'rl'] = df_rl_cy[0].values
+    temps_cy.loc[:,'rh'] = df_rh_cy[0].values
+    temps_cy.loc[:,'nh'] = df_norms_cy[3].values
+    temps_cy.loc[:,'nl'] = df_norms_cy[4].values
+   
+    if period == 'spring':
+        temps = temps_cy[temps_cy.index.month.isin([3,4,5])]
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
+      
+    elif period == 'summer':
+        temps = temps_cy[temps_cy.index.month.isin([6,7,8])]
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
+
+    elif period == 'fall':
+        temps = temps_cy[temps_cy.index.month.isin([9,10,11])]
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
+
+    elif period == 'winter':
+        date_range = []
+        date_time = []
+        sdate = date(int(previous_year), 12, 1)
+        edate = date(int(selected_year), 12, 31)
+
+        delta = edate - sdate
+
+        for i in range(delta.days + 1):
+            day = sdate + timedelta(days=i)
+            date_range.append(day)
+        for j in date_range:
+            day = j.strftime("%Y-%m-%d")
+            date_time.append(day)
+
+        temps_py = temps_py[temps_py.index.month.isin([12])]
+        temps_cy = temps_cy[temps_cy.index.month.isin([1,2])]
+        temp_frames = [temps_py, temps_cy]
+        temps = pd.concat(temp_frames, sort=True)
+        date_time = date_time[:91]  
+        
+        df_record_highs_jan_feb = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(01-)|(02-)')]
+        df_record_highs_dec = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(12-)')]
+        high_frames = [df_record_highs_dec, df_record_highs_jan_feb]
+        df_record_highs = pd.concat(high_frames)
+
+        df_record_lows_jan_feb = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(01-)|(02-)')]
+        df_record_lows_dec = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(12-)')]
+        low_frames = [df_record_lows_dec, df_record_lows_jan_feb]
+        df_record_lows = pd.concat(low_frames)
+
+        df_high_norms_jan_feb = df_norms[3][0:60]
+        df_high_norms_dec = df_norms[3][335:]
+        high_norm_frames = [df_high_norms_dec, df_high_norms_jan_feb]
+        df_high_norms = pd.concat(high_norm_frames)
+
+        df_low_norms_jan_feb = df_norms[4][0:60]
+        df_low_norms_dec = df_norms[4][335:]
+        low_norm_frames = [df_low_norms_dec, df_low_norms_jan_feb]
+        df_low_norms = pd.concat(low_norm_frames)
+
+        bar_x = date_time
+        nh_value = df_high_norms
+        nl_value = df_low_norms
+        rh_value = df_record_highs[0]
+        rl_value = df_record_lows[0]
+
+    else:
+        temps = temps_cy
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
+
+    mkr_color = {'color':'black'}
+      
+    trace = [
+            go.Bar(
+                y = temps['dif'],
+                x = bar_x,
+                base = temps['TMIN'],
+                name='Temp Range',
+                marker = mkr_color,
+                hovertemplate = 'Temp Range: %{y} - %{base}<extra></extra><br>'
+                                # 'Record High: %{temps[6]}'                  
+            ),
+            go.Scatter(
+                y = nh_value,
+                x = bar_x,
+                # hoverinfo='none',
+                name='Normal High',
+                marker = {'color':'indianred'}
+            ),
+            go.Scatter(
+                y = nl_value,
+                x = bar_x,
+                # hoverinfo='none',
+                name='Normal Low',
+                marker = {'color':'slateblue'}
+            ),
+            go.Scatter(
+                y = rh_value,
+                x = bar_x,
+                # hoverinfo='none',
+                name='Record High',
+                marker = {'color':'red'}
+            ),
+            go.Scatter(
+                y = rl_value,
+                x = bar_x,
+                # hoverinfo='none',
+                name='Record Low',
+                marker = {'color':'blue'}
+            ),
+        ]
+    layout = go.Layout(
+                xaxis = {'rangeslider': {'visible':True},},
+                yaxis = {"title": 'Temperature F'},
+                title ='Daily Temps',
+                plot_bgcolor = 'lightgray',
+                height = 700,
+        )
+    return {'data': trace, 'layout': layout}
+
+
+
+
+@app.callback(
+    Output('daily-max-t', 'children'),
+    [Input('product', 'value'),
+    Input('d-max-max', 'children'),
+    Input('avg-of-dly-highs', 'children'),
+    Input('d-min-max', 'children')])
 def max_stats(product, d_max_max, admaxh, d_min_max):
     dly_max_max = d_max_max
     admaxh = admaxh
@@ -393,7 +587,6 @@ def climate_day_graph(selected_date, all_data, selected_param, selected_product)
     Output('bar', 'children'),
     [Input('product', 'value' )])
 def display_day_bar(selected_product):
-    print(selected_product)
     if selected_product == 'climate-for-day':
         return dcc.Graph(id='climate-day-bar')
 
@@ -492,179 +685,7 @@ def display_year_selector(product_value):
                 )
 
 
-@app.callback(Output('graph1', 'figure'),
-             [Input('temp-data', 'children'),
-             Input('rec-highs', 'children'),
-             Input('rec-lows', 'children'),
-             Input('norms', 'children'),
-             Input('year', 'value'),
-             Input('period', 'value')])
-def update_figure(temp_data, rec_highs, rec_lows, norms, selected_year, period):
-    # print(period)
-    previous_year = int(selected_year) - 1
-    selected_year = selected_year
-    print(type(selected_year))
-    temps = pd.read_json(temp_data)
-    temps = temps.drop([0,1], axis=1)
-    temps.columns = ['date','TMAX','TMIN']
-    print(type(temps['date']))
-    # temps['date']
-    temps['date'] = pd.to_datetime(temps['date'], unit='ms')
-    temps = temps.set_index(['date'])
-    temps['dif'] = temps['TMAX'] - temps['TMIN']
-    
-    # print(type(temps['date'][0]))
-    # print(temps['date'])
-   
-    # temps_cy = temps[temps.index.year.isin([selected_year])]
-    temps_cy = temps[(temps.index.year==selected_year)]
-    # temps_py = temps[temps.index.year.isin([previous_year])]
-    temps_py = temps[(temps.index.year==previous_year)]
-    df_record_highs_ly = pd.read_json(rec_highs)
-    df_record_highs_ly = df_record_highs_ly.set_index(1)
-    df_record_lows_ly = pd.read_json(rec_lows)
-    df_record_lows_ly = df_record_lows_ly.set_index(1)
-    df_rl_cy = df_record_lows_ly[:len(temps_cy.index)]
-    df_rh_cy = df_record_highs_ly[:len(temps_cy.index)]
-    print(temps_cy)
 
-    
-    df_norms = pd.read_json(norms)
-    df_norms_cy = df_norms[:len(temps_cy.index)]
-  
-    temps_cy.loc[:,'rl'] = df_rl_cy[0].values
-    temps_cy.loc[:,'rh'] = df_rh_cy[0].values
-    temps_cy.loc[:,'nh'] = df_norms_cy[3].values
-    temps_cy.loc[:,'nl'] = df_norms_cy[4].values
-   
-    if period == 'spring':
-        temps = temps_cy[temps_cy.index.month.isin([3,4,5])]
-        nh_value = temps['nh']
-        nl_value = temps['nl']
-        rh_value = temps['rh']
-        rl_value = temps['rl']
-        bar_x = temps.index
-      
-    elif period == 'summer':
-        temps = temps_cy[temps_cy.index.month.isin([6,7,8])]
-        nh_value = temps['nh']
-        nl_value = temps['nl']
-        rh_value = temps['rh']
-        rl_value = temps['rl']
-        bar_x = temps.index
-
-    elif period == 'fall':
-        temps = temps_cy[temps_cy.index.month.isin([9,10,11])]
-        nh_value = temps['nh']
-        nl_value = temps['nl']
-        rh_value = temps['rh']
-        rl_value = temps['rl']
-        bar_x = temps.index
-
-    elif period == 'winter':
-        date_range = []
-        date_time = []
-        sdate = date(int(previous_year), 12, 1)
-        edate = date(int(selected_year), 12, 31)
-
-        delta = edate - sdate
-
-        for i in range(delta.days + 1):
-            day = sdate + timedelta(days=i)
-            date_range.append(day)
-        for j in date_range:
-            day = j.strftime("%Y-%m-%d")
-            date_time.append(day)
-
-        temps_py = temps_py[temps_py.index.month.isin([12])]
-        temps_cy = temps_cy[temps_cy.index.month.isin([1,2])]
-        temp_frames = [temps_py, temps_cy]
-        temps = pd.concat(temp_frames, sort=True)
-        date_time = date_time[:91]  
-        
-        df_record_highs_jan_feb = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(01-)|(02-)')]
-        df_record_highs_dec = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(12-)')]
-        high_frames = [df_record_highs_dec, df_record_highs_jan_feb]
-        df_record_highs = pd.concat(high_frames)
-
-        df_record_lows_jan_feb = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(01-)|(02-)')]
-        df_record_lows_dec = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(12-)')]
-        low_frames = [df_record_lows_dec, df_record_lows_jan_feb]
-        df_record_lows = pd.concat(low_frames)
-
-        df_high_norms_jan_feb = df_norms[3][0:60]
-        df_high_norms_dec = df_norms[3][335:]
-        high_norm_frames = [df_high_norms_dec, df_high_norms_jan_feb]
-        df_high_norms = pd.concat(high_norm_frames)
-
-        df_low_norms_jan_feb = df_norms[4][0:60]
-        df_low_norms_dec = df_norms[4][335:]
-        low_norm_frames = [df_low_norms_dec, df_low_norms_jan_feb]
-        df_low_norms = pd.concat(low_norm_frames)
-
-        bar_x = date_time
-        nh_value = df_high_norms
-        nl_value = df_low_norms
-        rh_value = df_record_highs[0]
-        rl_value = df_record_lows[0]
-
-    else:
-        temps = temps_cy
-        nh_value = temps['nh']
-        nl_value = temps['nl']
-        rh_value = temps['rh']
-        rl_value = temps['rl']
-        bar_x = temps.index
-
-    mkr_color = {'color':'black'}
-      
-    trace = [
-            go.Bar(
-                y = temps['dif'],
-                x = bar_x,
-                base = temps['TMIN'],
-                name='Temp Range',
-                marker = mkr_color,
-                hovertemplate = 'Temp Range: %{y} - %{base}<extra></extra><br>'
-                                # 'Record High: %{temps[6]}'                  
-            ),
-            go.Scatter(
-                y = nh_value,
-                x = bar_x,
-                # hoverinfo='none',
-                name='Normal High',
-                marker = {'color':'indianred'}
-            ),
-            go.Scatter(
-                y = nl_value,
-                x = bar_x,
-                # hoverinfo='none',
-                name='Normal Low',
-                marker = {'color':'slateblue'}
-            ),
-            go.Scatter(
-                y = rh_value,
-                x = bar_x,
-                # hoverinfo='none',
-                name='Record High',
-                marker = {'color':'red'}
-            ),
-            go.Scatter(
-                y = rl_value,
-                x = bar_x,
-                # hoverinfo='none',
-                name='Record Low',
-                marker = {'color':'blue'}
-            ),
-        ]
-    layout = go.Layout(
-                xaxis = {'rangeslider': {'visible':True},},
-                yaxis = {"title": 'Temperature F'},
-                title ='Daily Temps',
-                plot_bgcolor = 'lightgray',
-                height = 700,
-        )
-    return {'data': trace, 'layout': layout}
 
 @app.callback(Output('fyma-graph', 'figure'),
              [Input('temp-param', 'value'),
@@ -745,26 +766,19 @@ def display_graph(value):
 @app.callback(Output('all-data', 'children'),
             [Input('product', 'value')])
 def all_temps_cleaner(product_value):
-    # print(product_value)
+  
     cleaned_all_temps = df_all_temps
     cleaned_all_temps.columns=['dow','sta','Date','TMAX','TMIN']
     # cleaned_all_temps['Date'] = pd.to_datetime(cleaned_all_temps['Date'])
     cleaned_all_temps = cleaned_all_temps.drop(['dow','sta'], axis=1)
-    print(type(cleaned_all_temps['Date']))
-    print(cleaned_all_temps['Date'][0])
-    # cleaned_all_temps["Date"] = pd.to_datetime(cleaned_all_temps['Date'], format = '%Y-%m-%d')
-    # cleaned_all_temps['Date'] = cleaned_all_temps['Date'].strftime('%Y-%m-%d')
-    # print(type(cleaned_all_temps['Date'][0]))
-    # print(cleaned_all_temps['Date'][0])
 
-    # return cleaned_all_temps.to_json(date_format='iso')
     return cleaned_all_temps.to_json()
 
 @app.callback(Output('title-date-range', 'children'),
             [Input('product', 'value'),
             Input('all-data', 'children')])
 def all_temps_cleaner(product, temps):
-    # print(temps)
+   
     title_temps = pd.read_json(temps)
     title_temps['Date'] = pd.to_datetime(title_temps['Date'], unit='ms')
     title_temps['Date']=title_temps['Date'].dt.strftime("%Y-%m-%d")
@@ -805,18 +819,15 @@ def norm_highs(selected_year):
     Input('product', 'value')])
 def clean_df5(all_data, product_value):
     dr = pd.read_json(all_data)
-    print(dr)
     dr['Date'] = pd.to_datetime(dr['Date'], unit='ms')
-    # df_all_temps['Date'] = pd.to_datetime(df_all_temps['Date'], unit='ms')
-    print(type(dr['Date'][0]))
-    # title_temps['Date'] = title_temps['Date'].dt.strftime("%Y-%m-%d")
+   
     df_date_index = df_all_temps.set_index(['Date'])
-    print(df_date_index)
+
     df_date_index.index = pd.to_datetime(df_date_index.index)
     df_ya_max = df_date_index.resample('Y').mean()
     df5 = df_ya_max[:-1]
     df5 = df5.drop(['dow'], axis=1)
-    print(df5)
+
     return df5.to_json(date_format='iso')
 
 @app.callback(
